c179fb7b9a0c8d0ab2ddbd84b30b4b2273763085
koladev <onaelmangabo@gmail.com>
2022-01-26 23:16:09 +0100

    - feat: add currency and wallet interfaces

--- src/models/currency.ts
+++ src/models/currency.ts
@@ -1,4 +1,4 @@
 export interface Currency {
   balance: number;
-  symbol: string
+  currency: string;
 }
---
+++ src/models/wallet.ts
@@ -0,0 +1,7 @@
+import { Currency } from "./currency";
+
+export interface Wallet {
+  EUR: Currency;
+  USD: Currency;
+  GBP: Currency;
+}
