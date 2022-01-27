e17a2ef135c97abf0cf29f969ab77eee3c3c437a
koladev <onaelmangabo@gmail.com>
2022-01-27 20:49:37 +0100

    - test: adding custom render for testing with redux

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "./utils/test-utils";
 import App from "./App";
+// so we can import fireEvent and screen here as well
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Currency exchange/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render as rtlRender } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { walletSlice } from "../store/slices/wallet";
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({ reducer: { wallet: walletSlice.reducer }, preloadedState }),
+    ...renderOptions
+  } = {},
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export * from "@testing-library/react";
+export { render };
