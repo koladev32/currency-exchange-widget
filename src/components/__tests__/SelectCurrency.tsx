ab0cb114eb0a4783904dc3891da00b2b4bc57a09
koladev <onaelmangabo@gmail.com>
2022-01-27 23:50:46 +0100

    - test: add currency select test

---
+++ src/components/__tests__/SelectCurrency.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "../../utils/test-utils";
+import { ISymbols, projectCurrencies, symbolsEmojis } from "../../utils/constants";
+import SelectCurrency from "../SelectCurrency";
+
+test("renders SelectCurrency", () => {
+  render(
+    <SelectCurrency
+      currencies={projectCurrencies}
+      onChange={() => {}}
+    />,
+  );
+  const optionEURElement = screen.getByText(symbolsEmojis[projectCurrencies[0] as keyof ISymbols]);
+  expect(optionEURElement).toBeInTheDocument();
+});
