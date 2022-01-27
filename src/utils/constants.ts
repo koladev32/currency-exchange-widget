e0b06cab8ba6e6709d4ce9ffb3755cd22afcd7f7
koladev <onaelmangabo@gmail.com>
2022-01-27 22:48:49 +0100

    - feat: add symbols utils

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,13 @@
 export const projectCurrencies = ["EUR", "USD", "GBP"];
+
+export type ISymbols = {
+  EUR: string;
+  USD: string;
+  GBP:string;
+}
+
+export const symbols:ISymbols = {
+  EUR: "€",
+  USD: "$",
+  GBP: "£",
+};
