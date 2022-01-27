4f55468d7ed376f7d47f142f377f38858d54ef84
koladev <onaelmangabo@gmail.com>
2022-01-27 21:08:57 +0100

    - chore: fix typo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen } from "./utils/test-utils";
 import App from "./App";
-// so we can import fireEvent and screen here as well
 
 test("renders learn react link", () => {
   render(<App />);
