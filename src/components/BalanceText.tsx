21de5802ad742d39125ed9d36c11554f35e5cfb3
koladev <onaelmangabo@gmail.com>
2022-01-28 01:50:53 +0100

    - feat: add balance text component

---
+++ src/components/BalanceText.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ISymbols, symbols } from "../utils/constants";
+
+interface IBalanceText {
+  balance: number|string;
+  currency: string;
+}
+
+const BalanceText: React.FC<IBalanceText> = ({ balance, currency }) => (
+  <p className="text-xs ml-2 pl-4 w-full">
+    Balance:
+    {symbols[currency as keyof ISymbols]}
+    {balance}
+  </p>
+);
+
+export default BalanceText;
