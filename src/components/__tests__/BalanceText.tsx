003d719a9883c73a6dd5a63fe63f397deedf08b8
koladev <onaelmangabo@gmail.com>
2022-01-28 01:50:42 +0100

    - test: add balance text component test

---
+++ src/components/__tests__/BalanceText.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, screen } from "../../utils/test-utils";
+import BalanceText from "../BalanceText";
+
+const fixtures = {
+  currency: "EUR",
+  balance: 1000,
+};
+
+test("renders BalanceText", () => {
+  render(
+    <BalanceText
+      currency={fixtures.currency}
+      balance={fixtures.balance}
+    />,
+  );
+  const balanceElement = screen.getByText(/Balance/i);
+  expect(balanceElement).toBeInTheDocument();
+});
